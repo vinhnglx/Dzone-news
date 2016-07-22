ef5cbe6eeab25b8f12c2167f029d0bca5d7a96d2
Vinh Nguyen <vinh@betacode.io>
2016-07-22 11:10:44 +0800

    Define gulp task sass to compile scss to css

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,20 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
+var sass = require('gulp-sass');
 
-gulp.task('default', function() {
+gulp.task('react', function() {
   return browserify('./source/app.js')
         .transform(babelify)
         .bundle()
         .pipe(source('dzone-news.js'))
         .pipe(gulp.dest('./build/'));
 });
+
+gulp.task('sass', function() {
+  return gulp.src('./source/app.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./build/'))
+});
+
+gulp.task('default', ['sass', 'react']);
