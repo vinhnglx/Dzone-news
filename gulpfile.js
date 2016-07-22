3067a0d9c07b5063bd8c2585e5fa99a64eab3b20
Vinh Nguyen <vinh@betacode.io>
2016-07-22 18:21:30 +0800

    Update Gulp tasks for compile css, html, js, image files

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,38 @@
-var gulp = require('gulp');
-var browserify = require('browserify');
-var babelify = require('babelify');
-var source = require('vinyl-source-stream');
-var sass = require('gulp-sass');
+var gulp = require('gulp'),
+    browserify = require('browserify'),
+    babelify = require('babelify'),
+    source = require('vinyl-source-stream'),
+    sass = require('gulp-sass'),
+    minifyCss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    streamify = require('gulp-streamify'),
+    minifyHtml = require('gulp-minify-html');
 
 gulp.task('react', function() {
-  return browserify('./source/app.js')
+  return browserify('./source/scripts/main.js')
         .transform(babelify)
         .bundle()
-        .pipe(source('dzone-news.js'))
-        .pipe(gulp.dest('./build/'));
+        .pipe(source('main.js'))
+        .pipe(streamify(uglify()))
+        .pipe(gulp.dest('./build/scripts/'));
 });
 
-gulp.task('sass', function() {
-  return gulp.src('./source/app.scss')
+gulp.task('scss', function() {
+  gulp.src(['./source/styles/main.scss'])
     .pipe(sass())
+    .pipe(minifyCss())
+    .pipe(gulp.dest('./build/styles/'))
+})
+
+gulp.task('index', function() {
+  return gulp.src('./source/index.html')
+    .pipe(minifyHtml())
     .pipe(gulp.dest('./build/'))
 });
 
-gulp.task('default', ['sass', 'react']);
+gulp.task('image', function() {
+  return gulp.src('./source/img/*.{jpg, png}')
+    .pipe(gulp.dest('./build/img'))
+});
+
+gulp.task('default', ['index', 'scss', 'react', 'image']);
