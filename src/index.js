1076e2abeddd4e3e6f30c65c225926d6a37c456d
Vinh Nguyen <vinh@betacode.io>
2016-08-11 22:48:47 +0800

    Configure category route

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import App from './components/Main';
+import Category from './components/Category';
 
-// Render the main component into the dom
-ReactDOM.render(<App />, document.getElementById('dzone-news'));
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={App} />
+    </Route>
+    <Route path="/categories/:category" component={Category} />
+  </Router>
+), document.getElementById('dzone-news'));
