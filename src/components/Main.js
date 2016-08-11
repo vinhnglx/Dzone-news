1aa05ff35a9045aae980dbcee6ab8ac3e97da586
Vinh Nguyen <vinh@betacode.io>
2016-08-11 22:48:37 +0800

    Implement load news by category

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,17 +3,24 @@ require('styles/App.scss');
 
 import React from 'react';
 import Stream from './Stream';
+import CategoryList from './CategoryList';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="row">
-        <div className="col-md-12">
-          <div className="blog-post">
-            <Stream />
+      <section className="row">
+        <article className="col-md-8">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="blog-post">
+                <Stream />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </article>
+
+        <CategoryList />
+      </section>
     );
   }
 }
--- src/components/News.js
+++ src/components/News.js
@@ -5,21 +5,31 @@ class News extends React.Component {
   render() {
     let news = this.props.entry;
     let desc = news.description;
-    console.log(news);
+    let categories;
+
+    categories = news.categories.map(function(category, index) {
+      return (
+        <a href="javascript:void(0);" className="tag-name" key={index}>
+          <span className="label label-default">{category}</span>
+        </a>
+      )
+    });
 
     return (
       <div>
-        <h4>
+        <div className="post-preview">
           <a href={news.link} target="_blank">
-            {news.title}
+            <h2 className="post-title">
+              {news.title}
+            </h2>
+            <h5 className="post-subtitle" dangerouslySetInnerHTML={{__html: desc}}></h5>
           </a>
-          <small>
-            <span>      Posted: </span>
-            <span className="post_time">{moment(news.pubDate).format('YYYY-MM-DD hh:mm:ss')}></span>
-          </small>
-        </h4>
 
-        <div className="content" dangerouslySetInnerHTML={{__html: desc}}></div>
+          <p>
+            {categories}
+          </p>
+          <p className="post-meta">Posted by {news.author} on {moment(news.pubDate).format('YYYY-MM-DD hh:mm:ss')}</p>
+        </div>
         <hr/>
       </div>
     )
